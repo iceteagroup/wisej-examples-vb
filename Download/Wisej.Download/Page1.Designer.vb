fd3d504af998d7e79e5b6a49ffedb9ddbe723cfa
Frank Boettcher <37643082+FrankITG@users.noreply.github.com>
2020-12-05 22:07:46 +0100

    Add files via upload

---
+++ Download/Wisej.Download/Page1.Designer.vb
@@ -0,0 +1,116 @@
+﻿Imports System
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Page1
+    Inherits Page
+
+
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As System.ComponentModel.IContainer
+
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+#Region "Wisej Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Page1))
+        Me.button1 = New Wisej.Web.Button()
+        Me.label1 = New Wisej.Web.Label()
+        Me.button2 = New Wisej.Web.Button()
+        Me.button3 = New Wisej.Web.Button()
+        Me.button4 = New Wisej.Web.Button()
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(38, 129)
+        Me.button1.Name = "button1"
+        Me.button1.Padding = New Wisej.Web.Padding(50, 0, 0, 0)
+        Me.button1.Size = New System.Drawing.Size(459, 36)
+        Me.button1.TabIndex = 0
+        Me.button1.Text = "1. Download PDF"
+        Me.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'label1
+        '
+        Me.label1.Location = New System.Drawing.Point(38, 38)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(459, 85)
+        Me.label1.TabIndex = 1
+        Me.label1.Text = resources.GetString("label1.Text")
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(38, 188)
+        Me.button2.Name = "button2"
+        Me.button2.Padding = New Wisej.Web.Padding(50, 0, 0, 0)
+        Me.button2.Size = New System.Drawing.Size(459, 36)
+        Me.button2.TabIndex = 2
+        Me.button2.Text = "2. Download and open PDF"
+        Me.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'button3
+        '
+        Me.button3.Location = New System.Drawing.Point(38, 247)
+        Me.button3.Name = "button3"
+        Me.button3.Padding = New Wisej.Web.Padding(50, 0, 0, 0)
+        Me.button3.Size = New System.Drawing.Size(459, 36)
+        Me.button3.TabIndex = 3
+        Me.button3.Text = "3. Download PDF with FileStream"
+        Me.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'button4
+        '
+        Me.button4.Location = New System.Drawing.Point(38, 306)
+        Me.button4.Name = "button4"
+        Me.button4.Padding = New Wisej.Web.Padding(50, 0, 0, 0)
+        Me.button4.Size = New System.Drawing.Size(459, 36)
+        Me.button4.TabIndex = 4
+        Me.button4.Text = "4. Download ZIP created in memory"
+        Me.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Page1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = Wisej.Web.AutoScaleMode.Font
+        Me.Controls.Add(Me.button4)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.button1)
+        Me.Name = "Page1"
+        Me.Size = New System.Drawing.Size(929, 583)
+        Me.Text = "Page1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+
+#End Region
+
+    Friend WithEvents button1 As Web.Button
+    Friend WithEvents label1 As Web.Label
+    Friend WithEvents button2 As Web.Button
+    Friend WithEvents button3 As Web.Button
+    Friend WithEvents button4 As Web.Button
+End Class
---
+++ Download/Wisej.Download/Page1.vb
@@ -0,0 +1,80 @@
+﻿Imports System
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Net.Mime
+Imports System.Web
+Imports Wisej.Core
+Imports Wisej.Web
+
+Partial Public Class Page1
+    Implements IWisejHandler
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+        ' provide normal download
+        Application.Download("Wisej-Datasheet-V2.2.pdf")
+    End Sub
+
+    Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button2.Click
+        ' provide download and open the file right away
+        ' use "_blank" to open in new tab - mostly blocked by modern browsers
+        Application.DownloadAndOpen("_self", "Wisej-Datasheet-V2.2.pdf")
+    End Sub
+
+    Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button3.Click
+        ' provide download based on a FileStream
+        Using stream As Stream = New FileStream(Path.Combine(Application.StartupPath, "Wisej-Datasheet-V2.2.pdf"), FileMode.Open)
+            Application.Download(stream, "PDF-FileStream-Download.pdf")
+        End Using
+    End Sub
+
+    Private Sub button4_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button4.Click
+        ' provide dynamic downloads fully handled in memory without temporary files
+        ' full control over what is returned to the browser
+        ' the GetPostbackURL is provided by the IWisejHandler interface
+        Application.Navigate(GetPostbackURL())
+    End Sub
+
+    Private Sub ProcessRequest(ByVal context As HttpContext) Implements IWisejHandler.ProcessRequest
+        ' create a ZIP file in memory
+        Dim memStream = New MemoryStream()
+
+        Using zip = New ZipArchive(memStream, ZipArchiveMode.Create, True)
+            zip.CreateEntryFromFile(Path.Combine(Application.StartupPath, "Wisej-Datasheet-V2.2.pdf"), "TestFile.pdf", CompressionLevel.Fastest)
+        End Using
+
+
+        ' reset the position in the stream
+        memStream.Position = 0
+
+        ' now that we have the ZIP, handle the request individually
+        Dim response = context.Response
+        response.AddHeader("Content-Type", "application/zip")
+
+        Dim oAux As ContentDisposition = New ContentDisposition()
+        oAux.FileName = "test.zip"
+        oAux.DispositionType = "inline"
+        response.AppendHeader("Content-Disposition", oAux.ToString())
+
+        'TODO Comment code not transpose automaticly from C#
+        'response.AppendHeader("Content-Disposition", New ContentDisposition(
+        '        ' provide a filename to use
+        '.FileName = "test.zip",
+        '        ' DispositionType can be "inline" or "attachment"
+        '        .DispositionType = "inline"
+        ').ToString())
+
+        ' now copy the stream with our ZIP file to the response output
+        memStream.CopyTo(response.OutputStream)
+    End Sub
+
+    Private ReadOnly Property Compress As Boolean Implements IWisejHandler.Compress
+        ' required for the IWisejHandler interface
+        Get
+            Return False
+        End Get
+    End Property
+End Class
