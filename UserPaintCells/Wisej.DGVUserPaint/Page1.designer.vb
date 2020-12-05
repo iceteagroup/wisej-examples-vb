1114a8830609aad9b6fc80eac9ec67a53d3ecf98
Frank Boettcher <37643082+FrankITG@users.noreply.github.com>
2020-12-05 22:27:57 +0100

    Add files via upload

---
+++ UserPaintCells/Wisej.DGVUserPaint/Page1.designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Page1
+    Inherits Wisej.Web.Page
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As ComponentModel.IContainer
+
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+#Region "Wisej Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        Me.label1 = New Wisej.Web.Label()
+        Me.dataGridView1 = New Wisej.Web.DataGridView()
+        Me.Column0 = New Wisej.Web.DataGridViewTextBoxColumn()
+        Me.Column2 = New Wisej.Web.DataGridViewComboBoxColumn()
+        Me.Column1 = New Wisej.Web.DataGridViewColumn()
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AllowHtml = True
+        Me.label1.Location = New System.Drawing.Point(40, 56)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(577, 116)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "<span style=""font-size:6em"">User Paint Cells</span>"
+        '
+        'dataGridView1
+        '
+        Me.dataGridView1.Anchor = CType((((Wisej.Web.AnchorStyles.Top Or Wisej.Web.AnchorStyles.Bottom) _
+            Or Wisej.Web.AnchorStyles.Left) _
+            Or Wisej.Web.AnchorStyles.Right), Wisej.Web.AnchorStyles)
+        Me.dataGridView1.Columns.AddRange(New Wisej.Web.DataGridViewColumn() {Me.Column0, Me.Column2, Me.Column1})
+        Me.dataGridView1.EditMode = Wisej.Web.DataGridViewEditMode.EditOnEnter
+        Me.dataGridView1.Location = New System.Drawing.Point(40, 178)
+        Me.dataGridView1.MinimumSize = New System.Drawing.Size(200, 350)
+        Me.dataGridView1.Name = "dataGridView1"
+        Me.dataGridView1.Size = New System.Drawing.Size(669, 374)
+        Me.dataGridView1.TabIndex = 1
+        '
+        'Column0
+        '
+        Me.Column0.HeaderText = "Values"
+        Me.Column0.Name = "Column0"
+        Me.Column0.Width = 300
+        '
+        'Column2
+        '
+        Me.Column2.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList
+        Me.Column2.HeaderText = "Type"
+        Me.Column2.Items.AddRange(New Object() {"Bar", "Area", "Line", "Spline", "Column"})
+        Me.Column2.Name = "Column2"
+        '
+        'Column1
+        '
+        Me.Column1.AutoSizeMode = Wisej.Web.DataGridViewAutoSizeColumnMode.Fill
+        Me.Column1.HeaderText = "Sparkline"
+        Me.Column1.MinimumWidth = 100
+        Me.Column1.Name = "Column1"
+        Me.Column1.UserPaint = True
+        '
+        'Page1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = Wisej.Web.AutoScaleMode.Font
+        Me.BackgroundImageLayout = Wisej.Web.ImageLayout.Zoom
+        Me.BackgroundImageSource = "Images/background.jpg"
+        Me.Controls.Add(Me.dataGridView1)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Page1"
+        Me.Size = New System.Drawing.Size(945, 626)
+        Me.Text = "Page1"
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+
+#End Region
+
+    Friend WithEvents label1 As Web.Label
+    Friend WithEvents dataGridView1 As Web.DataGridView
+    Friend WithEvents Column0 As Web.DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As Web.DataGridViewColumn
+    Friend WithEvents Column2 As Web.DataGridViewComboBoxColumn
+End Class
---
+++ UserPaintCells/Wisej.DGVUserPaint/Page1.vb
@@ -0,0 +1,83 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Drawing
+Imports System.Web.UI.DataVisualization.Charting
+Imports Wisej.Web
+
+Partial Public Class Page1
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub Page1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        dataGridView1.RowCount = 10
+
+        For Each row In dataGridView1.Rows
+            row(0).Value = "1,2,3,4,5,6,7,8,9,10"
+            row(1).Value = "Line"
+        Next
+    End Sub
+
+    Private Sub dataGridView1_CellValueChanged(ByVal sender As Object, ByVal e As DataGridViewCellEventArgs) Handles dataGridView1.CellValueChanged
+        If e.RowIndex > -1 Then
+
+            If e.ColumnIndex = 0 OrElse e.ColumnIndex = 1 Then
+                dataGridView1(2, e.RowIndex).Update()
+            End If
+        End If
+    End Sub
+
+    Private Sub dataGridView1_CellPaint(ByVal sender As Object, ByVal e As DataGridViewCellPaintEventArgs) Handles dataGridView1.CellPaint
+        If e.ColumnIndex = 2 AndAlso e.RowIndex > -1 Then
+            ' e.Graphics.FillRectangle(Brushes.White, e.ClipRectangle);
+
+            Dim text = TryCast(dataGridView1(0, e.RowIndex).Value, String)
+
+            If Not Equals(text, Nothing) Then
+                Dim values = text.Split(","c).[Select](Function(n) Convert.ToInt32(n)).ToArray()
+                Dim charType = CType([Enum].Parse(GetType(SeriesChartType), CStr(dataGridView1(1, e.RowIndex).Value)), SeriesChartType)
+                DrawChart(e.Graphics, e.ClipRectangle, values, charType)
+            End If
+        End If
+    End Sub
+
+    Private Sub DrawChart(ByVal g As Graphics, ByVal rect As Rectangle, ByVal values As Integer(), ByVal chartType As SeriesChartType)
+        Using chart = New Chart()
+            rect.Inflate(-10, -10)
+            chart.Width = rect.Width - 2
+            chart.Height = rect.Height - 2
+            chart.BackColor = Color.Transparent
+            Dim ca = New ChartArea()
+            ca.BackColor = Color.Transparent
+            ca.Position = New ElementPosition(1, 1, 99, 99)
+            ca.AxisY.LineWidth = 0
+            ca.AxisY.MajorGrid.LineWidth = 0
+            ca.AxisY.LabelStyle.Enabled = False
+            ca.AxisY.MajorTickMark.Enabled = False
+            ca.AxisY.MinorTickMark.Enabled = False
+            ca.AxisY.IsMarginVisible = False
+            ca.AxisX.LineWidth = 0
+            ca.AxisX.MajorGrid.LineWidth = 0
+            ca.AxisX.LabelStyle.Enabled = False
+            ca.AxisX.MajorTickMark.Enabled = False
+            ca.AxisX.MinorTickMark.Enabled = False
+            ca.AxisX.LineDashStyle = ChartDashStyle.NotSet
+            ca.AxisX.IsMarginVisible = False
+            chart.ChartAreas.Add(ca)
+            Dim s = New Series()
+            s.BorderWidth = 2
+            s.ChartType = chartType
+
+            For i As Integer = 0 To values.Length - 1
+                Dim p As DataPoint = New DataPoint()
+                p.XValue = i
+                p.YValues = New Double() {values(i)}
+                s.Points.Add(p)
+            Next
+
+            chart.Series.Add(s)
+            chart.Paint(g, rect)
+        End Using
+    End Sub
+End Class
