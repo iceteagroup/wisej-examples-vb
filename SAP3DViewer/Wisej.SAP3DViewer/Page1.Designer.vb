2627630c798ae3fcac15d628b8efe9adf1fb383d
Frank Boettcher <37643082+FrankITG@users.noreply.github.com>
2020-12-05 22:36:19 +0100

    Add files via upload

---
+++ SAP3DViewer/Wisej.SAP3DViewer/Page1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Page1
+    Inherits Wisej.Web.Page
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As ComponentModel.IContainer
+
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+#Region "Wisej Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim FileSystemProvider1 As Wisej.Core.FileSystemProvider = New Wisej.Core.FileSystemProvider()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Page1))
+        Me.button1 = New Wisej.Web.Button()
+        Me.openFileDialog = New Wisej.Web.OpenFileDialog(Me.components)
+        Me.comboBox1 = New Wisej.Web.ComboBox()
+        Me.button2 = New Wisej.Web.Button()
+        Me.comboBox2 = New Wisej.Web.ComboBox()
+        Me.upload1 = New Wisej.Web.Upload()
+        Me.groupBox1 = New Wisej.Web.GroupBox()
+        Me.label1 = New Wisej.Web.Label()
+        Me.flowLayoutPanel1 = New Wisej.Web.FlowLayoutPanel()
+        Me.flexLayoutPanel2 = New Wisej.Web.FlexLayoutPanel()
+        Me.widget1 = New Wisej.SAP3DViewer.VdsViewer()
+        Me.flexLayoutPanel1 = New Wisej.Web.FlexLayoutPanel()
+        Me.groupBox1.SuspendLayout()
+        Me.flowLayoutPanel1.SuspendLayout()
+        Me.flexLayoutPanel2.SuspendLayout()
+        Me.flexLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(3, 101)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(496, 57)
+        Me.button1.TabIndex = 1
+        Me.button1.Text = "Load Server File"
+        '
+        'openFileDialog
+        '
+        Me.openFileDialog.Filter = "VDS (*.vds)|*.vds|All files (*.*)|*.*"
+        Me.openFileDialog.InitialDirectory = "Project Files"
+        FileSystemProvider1.Name = "Project Files"
+        FileSystemProvider1.Root = "~\Samples\"
+        Me.openFileDialog.Roots.Add(FileSystemProvider1)
+        Me.openFileDialog.Title = "Select a VDS file."
+        '
+        'comboBox1
+        '
+        Me.comboBox1.Anchor = CType(((Wisej.Web.AnchorStyles.Top Or Wisej.Web.AnchorStyles.Left) _
+            Or Wisej.Web.AnchorStyles.Right), Wisej.Web.AnchorStyles)
+        Me.comboBox1.AutoSize = False
+        Me.comboBox1.DisplayMember = "name"
+        Me.comboBox1.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList
+        Me.comboBox1.Enabled = False
+        Me.comboBox1.Location = New System.Drawing.Point(16, 34)
+        Me.comboBox1.Name = "comboBox1"
+        Me.comboBox1.Size = New System.Drawing.Size(474, 43)
+        Me.comboBox1.TabIndex = 2
+        Me.comboBox1.ValueMember = "id"
+        '
+        'button2
+        '
+        Me.button2.Anchor = CType((Wisej.Web.AnchorStyles.Top Or Wisej.Web.AnchorStyles.Right), Wisej.Web.AnchorStyles)
+        Me.button2.Display = Wisej.Web.Display.Icon
+        Me.button2.Enabled = False
+        Me.button2.ImageSource = "node-closed"
+        Me.button2.Location = New System.Drawing.Point(443, 100)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(47, 43)
+        Me.button2.TabIndex = 3
+        Me.button2.Text = "Play"
+        '
+        'comboBox2
+        '
+        Me.comboBox2.Anchor = CType(((Wisej.Web.AnchorStyles.Top Or Wisej.Web.AnchorStyles.Left) _
+            Or Wisej.Web.AnchorStyles.Right), Wisej.Web.AnchorStyles)
+        Me.comboBox2.AutoSize = False
+        Me.comboBox2.DisplayMember = "name"
+        Me.comboBox2.DropDownStyle = Wisej.Web.ComboBoxStyle.DropDownList
+        Me.comboBox2.Enabled = False
+        Me.comboBox2.Location = New System.Drawing.Point(16, 100)
+        Me.comboBox2.Name = "comboBox2"
+        Me.comboBox2.Size = New System.Drawing.Size(421, 43)
+        Me.comboBox2.TabIndex = 4
+        Me.comboBox2.ValueMember = "id"
+        '
+        'upload1
+        '
+        Me.upload1.HideValue = True
+        Me.upload1.Location = New System.Drawing.Point(3, 35)
+        Me.upload1.Name = "upload1"
+        Me.upload1.Size = New System.Drawing.Size(496, 50)
+        Me.upload1.TabIndex = 5
+        Me.upload1.Text = "Load Client File"
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.comboBox2)
+        Me.groupBox1.Controls.Add(Me.button2)
+        Me.groupBox1.Controls.Add(Me.comboBox1)
+        Me.groupBox1.Location = New System.Drawing.Point(3, 174)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(496, 173)
+        Me.groupBox1.TabIndex = 6
+        Me.groupBox1.Text = "Play Steps"
+        '
+        'label1
+        '
+        Me.label1.Location = New System.Drawing.Point(3, 3)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(496, 25)
+        Me.label1.TabIndex = 7
+        Me.label1.Text = "Drag & Drop a VDS file"
+        Me.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'flowLayoutPanel1
+        '
+        Me.flowLayoutPanel1.AutoSize = True
+        Me.flowLayoutPanel1.AutoSizeMode = Wisej.Web.AutoSizeMode.GrowAndShrink
+        Me.flowLayoutPanel1.Controls.Add(Me.flexLayoutPanel2)
+        Me.flowLayoutPanel1.Controls.Add(Me.flexLayoutPanel1)
+        Me.flowLayoutPanel1.Dock = Wisej.Web.DockStyle.Top
+        Me.flowLayoutPanel1.Location = New System.Drawing.Point(5, 5)
+        Me.flowLayoutPanel1.Name = "flowLayoutPanel1"
+        Me.flowLayoutPanel1.ResponsiveProfiles.Add(CType(resources.GetObject("flowLayoutPanel1.ResponsiveProfiles"), Wisej.Base.ResponsiveProfile))
+        Me.flowLayoutPanel1.Size = New System.Drawing.Size(1016, 647)
+        Me.flowLayoutPanel1.TabIndex = 8
+        Me.flowLayoutPanel1.TabStop = True
+        '
+        'flexLayoutPanel2
+        '
+        Me.flexLayoutPanel2.AutoSize = True
+        Me.flexLayoutPanel2.Controls.Add(Me.label1)
+        Me.flexLayoutPanel2.Controls.Add(Me.widget1)
+        Me.flowLayoutPanel1.SetFillWeight(Me.flexLayoutPanel2, 1)
+        Me.flexLayoutPanel2.LayoutStyle = Wisej.Web.FlexLayoutStyle.Vertical
+        Me.flexLayoutPanel2.Location = New System.Drawing.Point(3, 3)
+        Me.flexLayoutPanel2.MinimumSize = New System.Drawing.Size(300, 0)
+        Me.flexLayoutPanel2.Name = "flexLayoutPanel2"
+        Me.flexLayoutPanel2.ResponsiveProfiles.Add(CType(resources.GetObject("flexLayoutPanel2.ResponsiveProfiles"), Wisej.Base.ResponsiveProfile))
+        Me.flexLayoutPanel2.ResponsiveProfiles.Add(CType(resources.GetObject("flexLayoutPanel2.ResponsiveProfiles1"), Wisej.Base.ResponsiveProfile))
+        Me.flexLayoutPanel2.Size = New System.Drawing.Size(502, 641)
+        Me.flexLayoutPanel2.Spacing = 4
+        Me.flexLayoutPanel2.TabIndex = 8
+        Me.flexLayoutPanel2.TabStop = True
+        '
+        'widget1
+        '
+        Me.widget1.AllowDrop = True
+        Me.widget1.Location = New System.Drawing.Point(3, 38)
+        Me.widget1.Name = "widget1"
+        Me.widget1.ResponsiveProfiles.Add(CType(resources.GetObject("widget1.ResponsiveProfiles"), Wisej.Base.ResponsiveProfile))
+        Me.widget1.ResponsiveProfiles.Add(CType(resources.GetObject("widget1.ResponsiveProfiles1"), Wisej.Base.ResponsiveProfile))
+        Me.widget1.Size = New System.Drawing.Size(496, 600)
+        Me.widget1.TabIndex = 0
+        Me.widget1.Text = "widget1"
+        '
+        'flexLayoutPanel1
+        '
+        Me.flexLayoutPanel1.AutoSize = True
+        Me.flexLayoutPanel1.AutoSizeMode = Wisej.Web.AutoSizeMode.GrowAndShrink
+        Me.flexLayoutPanel1.Controls.Add(Me.upload1)
+        Me.flexLayoutPanel1.Controls.Add(Me.button1)
+        Me.flexLayoutPanel1.Controls.Add(Me.groupBox1)
+        Me.flexLayoutPanel1.CssStyle = "transition:top 150ms, left 150ms"
+        Me.flowLayoutPanel1.SetFillWeight(Me.flexLayoutPanel1, 1)
+        Me.flexLayoutPanel1.LayoutStyle = Wisej.Web.FlexLayoutStyle.Vertical
+        Me.flexLayoutPanel1.Location = New System.Drawing.Point(511, 3)
+        Me.flexLayoutPanel1.MinimumSize = New System.Drawing.Size(300, 0)
+        Me.flexLayoutPanel1.Name = "flexLayoutPanel1"
+        Me.flexLayoutPanel1.Padding = New Wisej.Web.Padding(0, 32, 0, 0)
+        Me.flexLayoutPanel1.Size = New System.Drawing.Size(502, 350)
+        Me.flexLayoutPanel1.TabIndex = 9
+        Me.flexLayoutPanel1.TabStop = True
+        '
+        'Page1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = Wisej.Web.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.Controls.Add(Me.flowLayoutPanel1)
+        Me.Name = "Page1"
+        Me.Padding = New Wisej.Web.Padding(5)
+        Me.ResponsiveProfiles.Add(CType(resources.GetObject("$this.ResponsiveProfiles"), Wisej.Base.ResponsiveProfile))
+        Me.ResponsiveProfiles.Add(CType(resources.GetObject("$this.ResponsiveProfiles1"), Wisej.Base.ResponsiveProfile))
+        Me.Size = New System.Drawing.Size(1026, 569)
+        Me.Text = "Page1"
+        Me.groupBox1.ResumeLayout(False)
+        Me.flowLayoutPanel1.ResumeLayout(False)
+        Me.flowLayoutPanel1.PerformLayout()
+        Me.flexLayoutPanel2.ResumeLayout(False)
+        Me.flexLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+
+#End Region
+
+    Friend WithEvents widget1 As VdsViewer
+    Friend WithEvents button1 As Web.Button
+    Friend WithEvents openFileDialog As Web.OpenFileDialog
+    Friend WithEvents comboBox1 As Web.ComboBox
+    Friend WithEvents button2 As Web.Button
+    Friend WithEvents comboBox2 As Web.ComboBox
+    Friend WithEvents upload1 As Web.Upload
+    Friend WithEvents groupBox1 As Web.GroupBox
+    Friend WithEvents label1 As Web.Label
+    Friend WithEvents flowLayoutPanel1 As Web.FlowLayoutPanel
+    Friend WithEvents flexLayoutPanel2 As Web.FlexLayoutPanel
+    Friend WithEvents flexLayoutPanel1 As Web.FlexLayoutPanel
+End Class
---
+++ SAP3DViewer/Wisej.SAP3DViewer/Page1.vb
@@ -0,0 +1,107 @@
+﻿Imports System
+Imports Wisej.Web
+Imports System.Web
+Imports System.IO
+
+Partial Public Class Page1
+
+    Private fileStream As Stream
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Protected Overrides Sub OnLoad(ByVal e As EventArgs)
+        OpenFileDialog.Roots(0).Root = Application.MapPath("~\Samples")
+        MyBase.OnLoad(e)
+    End Sub
+
+    Private Async Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+        If Equals(Application.Browser.Device, "Mobile") Then openFileDialog.WindowState = FormWindowState.Maximized
+
+        If Await openFileDialog.ShowDialogAsync() = DialogResult.OK Then
+            ' The widget will issue a request to download the file using the special postback url 
+            ' to route the request bacck to the specific instance of the widget.
+            widget1.Call("loadFile", openFileDialog.FileName)
+        End If
+    End Sub
+
+    Private Sub widget1_WebRequest(ByVal sender As Object, ByVal e As WebRequestEventArgs)
+        ' Wisej.Web.Widget is able to act as a request handler and return any content.
+        Select Case e.Request("action")
+            Case "load"
+                Dim file = e.Request("file")
+
+                If Equals(file, "*") Then
+
+                    ' return the stream.
+                    If fileStream IsNot Nothing Then
+                        fileStream.CopyTo(e.Response.OutputStream)
+                        e.Response.Flush()
+                        fileStream = Nothing
+                    End If
+                Else
+                    ' map the path on the server so we don't send unsafe full paths to the browser.
+                    Dim path = openFileDialog.MapPath(file)
+                    e.Response.TransmitFile(path)
+                End If
+        End Select
+    End Sub
+
+    Private Async Sub widget1_WidgetEvent(ByVal sender As Object, ByVal e As WidgetEventArgs)
+        Select Case e.Type
+            Case "loaded"
+
+                ' load the procedures in the combobox once the file is loaded.
+                'Dim list As Dynamic() = Await widget1.CallAsync("readProcedures")
+                Dim list As Object = Await widget1.CallAsync("readProcedures")
+
+                comboBox1.Enabled = True
+                comboBox1.Items.Clear()
+                comboBox1.Items.AddRange(list)
+                If list.Length > 0 Then comboBox1.SelectedIndex = 0
+            Case "nodeSelected"
+                AlertBox.Show($"Selected Node: {e.Data}")
+        End Select
+    End Sub
+
+    Private Async Sub comboBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs)
+        comboBox2.Items.Clear()
+
+        If comboBox1.SelectedIndex > -1 Then
+            ' load the steps in the combobox when a procedure is selected.
+            'Dim list As Dynamic() = Await widget1.CallAsync("readSteps", CType(comboBox1.SelectedItem, Dynamic).id)
+            Dim list As Object = Await widget1.CallAsync("readSteps", CType(comboBox1.SelectedItem, Object).id)
+
+            button2.Enabled = True
+            comboBox2.Enabled = True
+            comboBox2.Items.AddRange(list)
+        End If
+    End Sub
+
+    Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button2.Click
+        'If comboBox2.SelectedIndex > -1 Then widget1.Call("playStep", CType(comboBox2.SelectedItem, Dynamic).id)
+        If comboBox2.SelectedIndex > -1 Then widget1.Call("playStep", CType(comboBox2.SelectedItem, Object).id)
+    End Sub
+
+    Private Sub widget1_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
+        e.AllowedFileTypes = ".vds"
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub widget1_DragDrop(ByVal sender As Object, ByVal e As DragEventArgs)
+        Dim files = CType(e.Data.GetData(DataFormats.Files), HttpFileCollection)
+        If files?.Count > 0 Then LoadFileStream(files(0).InputStream)
+    End Sub
+
+    Private Sub upload1_Uploaded(ByVal sender As Object, ByVal e As UploadedEventArgs) Handles upload1.Uploaded
+        If e.Files.Count > 0 Then LoadFileStream(e.Files(0).InputStream)
+    End Sub
+
+    Private Sub LoadFileStream(ByVal stream As Stream)
+        fileStream = New MemoryStream()
+        stream.CopyTo(fileStream)
+        fileStream.Position = 0
+        widget1.Call("loadFile", "*")
+    End Sub
+End Class
