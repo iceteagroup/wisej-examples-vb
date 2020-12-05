7526ebd42a0f1c3f211aaf1690b1251994f90fd0
Frank Boettcher <37643082+FrankITG@users.noreply.github.com>
2020-12-05 21:49:57 +0100

    Add files via upload

---
+++ Buttons/Wisej.Buttons/Page1.vb
@@ -0,0 +1,11 @@
+﻿Imports Wisej.Web
+
+Namespace Wisej.Buttons
+    Public Partial Class Page1
+        Inherits Page
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
---
+++ Buttons/Wisej.Buttons/Program.vb
@@ -0,0 +1,18 @@
+﻿Namespace Wisej.Buttons
+    Friend Module Program
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        Private Sub Main()
+            Call New Page1().Show()
+        End Sub
+
+        '
+        ' You can use the entry method below
+        ' to receive the parameters from the URL in the args collection.
+        '
+        'static void Main(NameValueCollection args)
+        '{
+        '}
+    End Module
+End Namespace
